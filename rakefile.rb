006397cdeceed43240dc296f2bcb4e1748c0c0c1
Kenji_Takahashi <wozniakk@gmail.com>
2012-01-26 13:33:22 +0100

    removed shouter

--- rakefile.rb
+++ rakefile.rb
@@ -9,25 +9,14 @@ task :cloud do
   site = Jekyll::Site.new(options)
   site.read_posts('')
 
-  shouter_html = ''
   blog_html = ''
 
   site.tags.sort.each do |tag, posts|
-    shout_s = posts.select {|p| p if p.data['type'] == 'shout'}.count
-    blog_s = posts.select {|p| p if p.data['type'] == 'post'}.count
-    shout_font_size = 12 + (shout_s * 1.3)
+    blog_s = posts.count
     blog_font_size = 12 + (blog_s * 1.3)
-    if shout_s > 0
-      shouter_html << "<a href=\"/tag/shouter/#{tag}.html\" title=\"Pages tagged #{tag}\" style=\"font-size: #{shout_font_size}px; line-height: #{shout_font_size}px\" rel=\"tag\">#{tag}</a>"
-    end
-    if blog_s > 0
-      blog_html << "<a href=\"/tag/blog/#{tag}.html\" title=\"Entires tagged #{tag}\" style=\"font-size: #{blog_font_size}px; line-height: #{blog_font_size}px\" rel=\"tag\">#{tag}</a>"
-    end
+    blog_html << "<a href=\"/tag/blog/#{tag}.html\" title=\"Entires tagged #{tag}\" style=\"font-size: #{blog_font_size}px; line-height: #{blog_font_size}px\" rel=\"tag\">#{tag}</a>\n"
   end
 
-  File.open('_includes/shouter_tags.html', 'w+') do |file|
-    file.puts shouter_html
-  end
   File.open('_includes/blog_tags.html', 'w+') do |file|
     file.puts blog_html
   end
@@ -46,25 +35,9 @@ task :tags do
   site = Jekyll::Site.new(options)
   site.read_posts('')
   site.tags.sort.each do |tag, posts|
-    shouts = posts.select {|p| p if p.data['type'] == 'shout'}
-    blogs = posts.select {|p| p if p.data['type'] == 'post'}
+    blogs = posts.count
     html = ''
-    if shouts.count > 0
-      html << <<-HTML
----
-type: shout
-layout: shouter
-title: Shouts tagged "#{tag}"
-tag: #{tag}
----
-HTML
-    end
-    FileUtils.mkdir_p "tag/shouter"
-    File.open("tag/shouter/#{tag}.html", 'w+') do |file|
-      file.puts html
-    end
-    html = ''
-    if blogs.count > 0
+    if blogs > 0
       html << <<-HTML
 ---
 type: blog
