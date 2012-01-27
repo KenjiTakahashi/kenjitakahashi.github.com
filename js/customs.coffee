0114dc5f045a2eaf0ecd58861cdd2a1566e4e7fe
Kenji_Takahashi <wozniakk@gmail.com>
2012-01-27 23:05:00 +0100

    more work on calendar

--- js/customs.coffee
+++ js/customs.coffee
@@ -1,3 +1,17 @@
+dates = {
+    '2012-01-01': ['sth']
+    '2012-01-04': []
+}
 $(document).ready( ->
-    $("#calendar").datepicker()
+    $("#calendar").datepicker({
+        showOtherMonths: true
+        selectOtherMonths: true
+        beforeShowDay: (date) ->
+            for d, _ of dates
+                if new Date(Date.parse(d)).setHours(0, 0, 0, 0) == date.setHours(0, 0, 0, 0)
+                    return [true, '']
+            return [false, '']
+        onSelect: (dateText, inst) ->
+            alert(new Date(dateText))
+    })
 )
--- js/customs.js
+++ js/customs.js
@@ -1,7 +1,29 @@
 (function() {
+  var dates;
+
+  dates = {
+    '2012-01-01': ['sth'],
+    '2012-01-04': []
+  };
 
   $(document).ready(function() {
-    return $("#calendar").datepicker();
+    return $("#calendar").datepicker({
+      showOtherMonths: true,
+      selectOtherMonths: true,
+      beforeShowDay: function(date) {
+        var d, _;
+        for (d in dates) {
+          _ = dates[d];
+          if (new Date(Date.parse(d)).setHours(0, 0, 0, 0) === date.setHours(0, 0, 0, 0)) {
+            return [true, ''];
+          }
+        }
+        return [false, ''];
+      },
+      onSelect: function(dateText, inst) {
+        return alert(new Date(dateText));
+      }
+    });
   });
 
 }).call(this);
